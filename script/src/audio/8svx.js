891318efd1b86fdcfd2a7546291e9e37782d7510
Stef Verheijen <dev@stef.be>
2026-05-08 18:42:26 +0200

    properly load Loop information from 8SVW samples.

--- script/src/audio/8svx.js
+++ script/src/audio/8svx.js
@@ -1,10 +1,13 @@
 export function read8SVXsample(file,sample){
 	// format description on http://wiki.amigaos.net/wiki/8SVX_IFF_8-Bit_Sampled_Voice
 
-	console.error("reading 8SVX sample");
+	console.log("reading 8SVX sample");
+
+	var oneShotHiSamples = 0;
+	var repeatHiSamples = 0;
 
 	// IFF file
-	function readChuck(){
+	function readChunk(){
 		var chunk = {};
 		chunk.name = file.readString(4);
 		chunk.size = file.readDWord();
@@ -14,25 +17,37 @@ export function read8SVXsample(file,sample){
 	file.litteEndian = false;
 	file.goto(12);
 
-	// look for BODY chunck
-	var chunk = readChuck();
+	// look for BODY chunk
+	var chunk = readChunk();
 	while (chunk.name != "BODY" && !file.isEOF(10)){
 
-		if (chunk.name == "NAME"){
+		if (chunk.name == "VHDR"){
+			oneShotHiSamples = file.readDWord();
+			repeatHiSamples  = file.readDWord();
+			file.jump(chunk.size - 8); // skip samplesPerHiCycle, samplesPerSec, ctOctave, sCompression, volume
+		} else if (chunk.name == "NAME"){
 			sample.name = file.readString(chunk.size);
-		}else{
+		} else {
 			file.jump(chunk.size);
-
-			// TODO: should we read the header to find loop repeat points?
-			// can't seem to find an example file that uses that.
 		}
-		chunk = readChuck();
+
+		// IFF chunks are padded to even byte boundaries
+		if (chunk.size & 1) file.jump(1);
+
+		chunk = readChunk();
 	}
 
 	if (chunk.name == "BODY"){
-		for (var j = 0; j<chunk.size; j++){
+		for (var j = 0; j < chunk.size; j++){
 			var b = file.readByte();
-			sample.data.push(b / 127)
+			sample.data.push(b / 128);
+		}
+
+		if (repeatHiSamples > 2){
+			sample.loop.start   = oneShotHiSamples;
+			sample.loop.length  = repeatHiSamples;
+			sample.loop.enabled = true;
+			sample.loop.type    = 1; // FORWARD
 		}
 	}
 }
